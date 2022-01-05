8eb5293cda82e90c0fd3675aa5c844f6183da671
ponteres05 <56670979+ponteres05@users.noreply.github.com>
2022-01-05 14:04:23 +0800

    Update invalidLoginCredentials.java

--- src/main/java/utility/invalidLoginCredentials.java
+++ src/main/java/utility/invalidLoginCredentials.java
@@ -4,5 +4,5 @@ public class invalidLoginCredentials {
     public static final String scenario = ["Invalid Username TC", "Invalid Password TC", "Blank Username and Password TC"];
     public static final String username = ["isaacstephen", "isaacstephen1@yahoo.com", ""];
     public static final String password = ["123456", "", ""];
-    public static final String expectedErrorMessage = "Please enter a valid email address.", "The credentials provided are incorrect", "No customer account found"];
+    public static final String expectedErrorMessage = ["Please enter a valid email address.", "The credentials provided are incorrect", "No customer account found"];
 }
