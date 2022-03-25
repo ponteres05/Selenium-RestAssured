cfaf906ec49f2a9557a0ba4dd747af0818c9ac66
ponteres05 <56670979+ponteres05@users.noreply.github.com>
2022-03-25 16:56:53 +0800

    Update invalidLoginCredentials.java

--- src/main/java/utility/invalidLoginCredentials.java
+++ src/main/java/utility/invalidLoginCredentials.java
@@ -1,8 +1,74 @@
 package utility;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class invalidLoginCredentials {
-    public static final String scenario = ["Invalid Username TC", "Invalid Password TC", "Blank Username and Password TC"];
-    public static final String username = ["isaacstephen", "isaacstephen1@yahoo.com", ""];
-    public static final String password = ["123456", "", ""];
-    public static final String expectedErrorMessage = ["Please enter a valid email address.", "The credentials provided are incorrect", "No customer account found"];
+
+    public ArrayList<String> getData(String scenarioName) throws IOException {
+        //fileInputStream argument
+        ArrayList<String> a=new ArrayList<String>();
+
+        FileInputStream fis=new FileInputStream("C://Users//Lenovo//Desktop//invalidLoginCredentials.xlsx");
+        XSSFWorkbook workbook=new XSSFWorkbook(fis);
+
+        int sheets=workbook.getNumberOfSheets();
+        for(int i=0;i<sheets;i++)
+        {
+            if(workbook.getSheetName(i).equalsIgnoreCase("testdata"))
+            {
+                XSSFSheet sheet = workbook.getSheetAt(i);
+
+                Iterator<Row> rows=sheet.iterator();
+                Row firstrow=rows.next();
+                Iterator<Cell> ce=firstrow.cellIterator();
+                int k=0;
+                int column=0;
+
+                while(ce.hasNext())
+                {
+                    Cell value=ce.next();
+                    if(value.getStringCellValue().equalsIgnoreCase("scenario"))
+                    {
+                        column=k;
+                    }
+
+                    k++;
+                }
+                System.out.println(column);
+
+                while(rows.hasNext())
+                {
+                    Row r=rows.next();
+                    if(r.getCell(column).getStringCellValue().equalsIgnoreCase(scenarioName))
+                    {
+                        Iterator<Cell> cv=r.cellIterator();
+                        while(cv.hasNext())
+                        {
+                            Cell c=cv.next();
+                            if(c.getCellType()==CellType.STRING)
+                            {
+                                a.add(c.getStringCellValue());
+                            }
+                            else
+                            {
+                                a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return a;
+    }
+
 }
