e5a712edad30365331402d9ed9fd606247b8baa0
ponteres05 <56670979+ponteres05@users.noreply.github.com>
2022-03-25 16:59:05 +0800

    Update DataDriven.java

--- src/test/java/com/webshop/DataDriven.java
+++ src/test/java/com/webshop/DataDriven.java
@@ -1,10 +1,14 @@
 package com.webshop;
 
-import com.webshop.pages.*;
+import com.webshop.pages.Common;
+import com.webshop.pages.LandingPage;
+import com.webshop.pages.Login;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class DataDriven {
 
@@ -16,22 +20,40 @@ public class DataDriven {
         this.driver = new ChromeDriver();
     }
 
-    @BeforeTest
-    public void openWebShopApplication(){
-        Common common = new Common(driver);
-
-        common.openApplication();
-    }
-
     @Test(priority = 1)
-
-    public void successfulLogin(){
+    public void invalidUsername() throws IOException {
+        Common common = new Common(driver);
         LandingPage landingPage = new LandingPage(driver);
         Login login = new Login(driver);
 
+        common.openApplication();
         landingPage.goToLoginPage();
-        login.fillInTheLoginFormAndClickLoginButton();
-        login.verifyThatCorrectErrormessageIsReceived();
+        login.fillInTheLoginFormAndClickLoginButton1();
+        login.verifyThatCorrectErrormessageIsReceived1();
     }
 
+    @Test(priority = 2)
+    public void invalidPassword() throws IOException {
+        Common common = new Common(driver);
+        LandingPage landingPage = new LandingPage(driver);
+        Login login = new Login(driver);
+
+        common.openApplication();
+        landingPage.goToLoginPage();
+        login.fillInTheLoginFormAndClickLoginButton2();
+        login.verifyThatCorrectErrormessageIsReceived2();
+    }
+
+    @Test(priority = 3)
+    public void blankUsernameAndPassword() throws IOException {
+        Common common = new Common(driver);
+        LandingPage landingPage = new LandingPage(driver);
+        Login login = new Login(driver);
+
+        common.openApplication();
+        landingPage.goToLoginPage();
+        login.fillInTheLoginFormAndClickLoginButton3();
+        login.verifyThatCorrectErrormessageIsReceived3();
+        common.closeApplication();
+    }
 }
